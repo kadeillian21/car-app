58c5322553d6d79409e43ded02a59aac71f897fd
Kade Illian <kadeillianmt@gmail.com>
2022-09-02 13:35:15 -0600

    added Jb functionality to the Car model

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,36 +1,36 @@
 class CarsController < ApplicationController
   def index
-    cars = Car.all
+    @cars = Car.all
 
-    render json: cars.as_json
+    render template: "cars/index"
   end
 
   def show
-    car = Car.find_by(id: params["id"])
+    @car = Car.find_by(id: params["id"])
 
-    render json: car.as_json
+    render template: "cars/show"
   end
 
   def create
-    car = Car.create(
+    @car = Car.create(
       make: params["make"],
       model: params["model"],
       year: params["year"],
       type_of_car: params["type_of_car"],
     )
 
-    render json: car.as_json
+    render template: "cars/show"
   end
 
   def update
-    car = Car.find_by(id: params["id"])
+    @car = Car.find_by(id: params["id"])
 
-    car.make = params["make"] || car.make
-    car.model = params["model"] || car.model
-    car.year = params["year"] || car.year
-    car.type_of_car = params["type_of_car"] || car.type_of_car
+    @car.make = params["make"] || @car.make
+    @car.model = params["model"] || @car.model
+    @car.year = params["year"] || @car.year
+    @car.type_of_car = params["type_of_car"] || @car.type_of_car
 
-    render json: car.as_json
+    render template: "cars/show"
   end
 
   def destroy
